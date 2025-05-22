53effc93ddab79e23d587f092f45c33eb9bd7ed3
Oreste Berta <oresteberta@Orestes-MacBook-Air-2.local>
2025-05-22 15:19:51 -0300

    Actualización después de cambio de usuario a ores999

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from .models import Publicacion
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 def lista_publicaciones(request):
     publicaciones = Publicacion.objects.all().order_by('-fecha_creacion')
     return render(request, 'blog/lista_publicaciones.html', {'publicaciones': publicaciones})
+
+    
