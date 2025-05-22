2e5800effa2178b3925c8a78027fd22f79895bd8
Oreste Berta <oresteberta@Orestes-MacBook-Air-2.local>
2025-05-22 15:32:46 -0300

    Actualización de archivos y configuración

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from .models import Publicacion
-from django.utils import timezone
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 def lista_publicaciones(request):
-    publicaciones = Publicacion.objects.all().order_by('-fecha_creacion')
-    return render(request, 'blog/lista_publicaciones.html', {'publicaciones': publicaciones})
-
-    
+    publicaciones = Publicacion.objects.filter(fecha_publicacion__lte=timezone.now())
+    usuarios = User.objects.all()
+    usuario_id = request.GET.get('usuario')
+    if usuario_id:
+        publicaciones = publicaciones.filter(autor__id=usuario_id)
+    return render(request, 'blog/lista_publicaciones.html', {
+        'publicaciones': publicaciones,
+        'usuarios': usuarios,
+        'usuario_activo': int(usuario_id) if usuario_id else None
+    })
